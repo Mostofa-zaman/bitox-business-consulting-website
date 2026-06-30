4355b2f38843615a80031042d25a10bb92559182
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-30 21:12:57 +0600

    feat(helper): create reusable icon provider

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import ButtonThree from "@/components/common/ButtonThree";
+import Responsive from "@/components/common/Responsive";
 
 
 
@@ -17,7 +18,12 @@ export default function Home() {
         <button className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
           Get Started
         </button>
+
+<Responsive.Flex>
+
 <ButtonThree/>
+
+</Responsive.Flex>
       </div>
     </main>
   );
---
+++ src/components/helper/iconProvider.jsx
@@ -0,0 +1,27 @@
+import { IoStarSharp } from "react-icons/io5";
+import { FaChevronRight } from "react-icons/fa6";
+import { FiPlus } from "react-icons/fi";
+import { RxTriangleRight } from "react-icons/rx";
+import { RxTriangleLeft } from "react-icons/rx";
+import { GiPlainSquare } from "react-icons/gi";
+import { GoDotFill } from "react-icons/go";
+import { GiTargetShot } from "react-icons/gi";
+import { IoChevronForwardSharp } from "react-icons/io5";
+import { FaChevronDown } from "react-icons/fa6";
+import { FaPlay } from "react-icons/fa";
+
+
+
+export const allIcons = {
+  star: <IoStarSharp />,
+  chevronRight: <FaChevronRight />,
+  plus: <FiPlus />,
+  triangleLeft: <RxTriangleLeft />,
+  triangleRight: <RxTriangleRight />,
+  square: <GiPlainSquare />,
+  dot: <GoDotFill />,
+  target: <GiTargetShot />,
+  chevronRight: <IoChevronForwardSharp />,
+  chebronDown :  <FaChevronDown/>,
+  play: <FaPlay/>
+};
