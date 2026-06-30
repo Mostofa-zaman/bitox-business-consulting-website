992da31cb45e7e1301540811529f7101c092e6bf
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-30 21:36:34 +0600

    feat(images): add feature, about, and brand assets

--- src/components/helper/imageProvider.jsx
+++ src/components/helper/imageProvider.jsx
@@ -2,6 +2,17 @@ import HeroImage from "../../../public/images/home01/hero/hero-card-icons.png";
 import HeroBanner1 from "../../../public/images/home01/hero/home-section-banner.png";
 import HeroBanner2 from "../../../public/images/home01/hero/home-section-banner2.png";
 import HeroBanner3 from "../../../public/images/home01/hero/home-section-banner3.jpg";
+import FeatureRoundOne from "../../../public/images/home01/feature-round/feature-round1.png";
+import FeatureRoundTwo from "../../../public/images/home01/feature-round/feature-round2.png";
+import AboutSectionOne from "../../../public/images/home01/about-section/home-about1.png";
+import AboutSectionTwo from "../../../public/images/home01/fabout-section/home-about2.png";
+import HomeBrandOne from "../../../public/images/home01/brand/brand1.png";
+import HomeBrandTwo from "../../../public/images/home01/brand/brand2.png";
+import HomeBrandThree from "../../../public/images/home01/brand/brand3.png";
+import HomeBrandFour from "../../../public/images/home01/brand/brand4.png";
+import HomeBrandFive from "../../../public/images/home01/brand/brand5.png";
+import HomeBrandSix from "../../../public/images/home01/brand/brand6.png";
+
 
 const allImages = {
   heroIcons: HeroImage,
@@ -10,6 +21,29 @@ const allImages = {
     { id: 1, img: HeroBanner1 },
     { id: 2, img: HeroBanner2 },
     { id: 3, img: HeroBanner3 },
+  ],
+    footerLogo: FooterLogo,
+  contact_logo: contact_logo,
+
+  featureRound: [
+    { id: 1, img: FeatureRoundOne },
+    { id: 2, img: FeatureRoundTwo },
+  ],
+  aboutsection: [
+    { id: 1, img: AboutSectionOne },
+    { id: 2, img: AboutSectionTwo },
+  ],
+  homeOneBrand: [
+    { id: 1, img: HomeBrandOne },
+    { id: 2, img: HomeBrandTwo },
+    { id: 3, img: HomeBrandThree },
+    { id: 4, img: HomeBrandFour },
+    { id: 5, img: HomeBrandFive },
+    { id: 6, img: HomeBrandSix },
+    { id: 7, img: HomeBrandOne },
+    { id: 8, img: HomeBrandTwo },
+    { id: 9, img: HomeBrandThree },
+    { id: 10, img: HomeBrandFour },
   ],
 };
 
