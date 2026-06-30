329645305dd9ca14baeb5455f7f0d2cfc028f9e3
mostofa zaman <mostofazaman1234@gmail.com>
2026-06-30 21:27:08 +0600

    feat(images): add hero image assets

--- src/components/helper/imageProvider.jsx
+++ src/components/helper/imageProvider.jsx
@@ -1,5 +1,16 @@
+import HeroImage from "../../../public/images/home01/hero/hero-card-icons.png";
+import HeroBanner1 from "../../../public/images/home01/hero/home-section-banner.png";
+import HeroBanner2 from "../../../public/images/home01/hero/home-section-banner2.png";
+import HeroBanner3 from "../../../public/images/home01/hero/home-section-banner3.jpg";
 
+const allImages = {
+  heroIcons: HeroImage,
 
-const allImages = {};
+  heroBanner: [
+    { id: 1, img: HeroBanner1 },
+    { id: 2, img: HeroBanner2 },
+    { id: 3, img: HeroBanner3 },
+  ],
+};
 
 export default allImages;
