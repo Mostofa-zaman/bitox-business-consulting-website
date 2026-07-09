6b24e91e16db1c7154be90937d5cc69dc2f06b72
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-09 23:03:18 +0600

    feat(footerTwo): create ContactCard component layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import ButtonThree from "@/components/common/ButtonThree";
 import Responsive from "@/components/common/Responsive";
 import FooterOne from "@/components/layout/FooterOne";
+import FooterTwo from "@/components/layout/FooterTwo";
 import NavbarOne from "@/components/layout/NavbarOne";
 import NavbarTwo from "@/components/layout/NavbarTwo";
 
@@ -13,8 +14,8 @@ export default function Home() {
 <Responsive.Flex>
 
 <NavbarOne/>
-<NavbarTwo/>
-<FooterOne/>
+
+<FooterTwo/>
 
 </Responsive.Flex>
    
--- src/components/layout/FooterTwo.jsx
+++ src/components/layout/FooterTwo.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client";
+  import { Stack } from "../common/Responsive";
 
-const FooterTwo = () => {
+
+
+// ─── Contact Form Card
+function ContactCard() {
   return (
-    <div>FooterTwo</div>
-  )
+    <div className="bg-bg-secondaryFore shadow-xl px-[38px] xl:px-[52px] py-[30px] xl:py-[45px] w-full lg:max-w-[390px] xl:max-w-[430px]">
+      <Stack gap="md">
+        <p className="headingFive font-bold text-primary leading-snug">
+          Have questions? Get in touch now!
+        </p>
+      </Stack>
+    </div>
+  );
 }
 
-export default FooterTwo
+
+
+// ─── Main Export
+export default function FooterTwo() {
+  return (
+    <div className="relative">
+      {/* Mobile: card in normal flow */}
+      <div className="lg:hidden flex justify-center  pb-0">
+        <ContactCard />
+      </div>
+
+   
+    
+    </div>
+  );
+}
