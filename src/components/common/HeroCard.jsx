8fb6e8f834865fa24d9cfcbde058b65706c86048
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-11 00:54:37 +0600

    feat(heroCard): add mobile responsive layout ,complete HeroCard

--- src/components/common/HeroCard.jsx
+++ src/components/common/HeroCard.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import allImages from "../helper/imageProvider";
 import ButtonTwo from "./ButtonTwo";
-import allImages from '../helper/imageProvider';
-
 
 const HeroCard = () => {
-     const { heroIocns } = allImages;
+  const { heroIocns } = allImages;
+
   return (
     <div>
-
-       <div className="max-w-[430px] p-[35px] bg-bg-secondaryTwo rounded-[6px] hidden  2xl:block">
+      {/* 2xl */}
+      <div className="max-w-[430px] p-[35px] bg-bg-secondaryTwo rounded-[6px] hidden  2xl:block">
         <h4 className="headingFour font-bold ">
           Building the right foundation of your business
         </h4>
@@ -63,7 +62,8 @@ const HeroCard = () => {
           </a>
         </div>
       </div>
-        {/* xl */}
+
+      {/* xl */}
       <div className="max-w-[430px] p-[35px] bg-bg-secondaryTwo rounded-[6px] hidden xl:block  2xl:hidden">
         <h4 className="headingFour font-bold ">
           Building the right foundation of your business
@@ -116,7 +116,9 @@ const HeroCard = () => {
           </a>
         </div>
       </div>
- <div className="max-w-[340px] p-[25px] bg-bg-secondaryTwo rounded-[6px] hidden lg:block xl:hidden">
+
+      {/* lg devices */}
+      <div className="max-w-[340px] p-[25px] bg-bg-secondaryTwo rounded-[6px] hidden lg:block xl:hidden">
         <h4 className="headingFour font-bold ">
           Building the right foundation of your business
         </h4>
@@ -167,12 +169,63 @@ const HeroCard = () => {
             </div>
           </a>
         </div>
-      </div> 
-      
+      </div>
+      {/* py-4 px-[25px] bg-primary text-start text-bg-secondaryTwo w-[100%]   rounded-[6px] */}
+      {/* sm, md devices */}
+      <div className="max-w-[470px] p-5 md:p-[35px] bg-bg-secondaryTwo rounded-[6px] block lg:hidden ">
+        <h4 className="headingFour font-bold ">
+          Building the right foundation of your business
+        </h4>
+        <p className="pt-[30px] pb-[50px] md:pb-[100px]  para-lg text-tarnary">
+          We are <span className="font-bold">dedicated to guiding</span> you on
+          your financial journey with expertise integrity and a personalized
+          approach, always.
+        </p>
 
-        
+        <div className="relative w-full">
+          <Link href={"/contact"}>
+            <ButtonTwo
+              frontText={"Get consultation"}
+              backText={"Get consultation"}
+              className={"!w-full !py-[18px] !justify-start !pr-[60px]"}
+            />
+          </Link>
+          <a
+            href="https://youtu.be/l6shgiK9Z-k?si=fMjmxKVUJythzBZ5"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            {/* Wave circle */}
+            <div className="absolute top-0 right-[3.5px] h-full flex items-center justify-center">
+              <div className="relative flex items-center justify-center w-[52px] h-[52px]">
+                <span
+                  className="absolute h-[50px] w-[50px] z-10 rounded-full bg-white/30 animate-wave"
+                  style={{ animationDelay: "0s" }}
+                />
+                <span
+                  className="absolute  h-[50px] w-[50px] z-10 rounded-full bg-white/30 animate-wave"
+                  style={{ animationDelay: "0.6s" }}
+                />
+                <span
+                  className="absolute  h-[50px] w-[50px] z-10 rounded-full bg-white/30 animate-wave"
+                  style={{ animationDelay: "1.2s" }}
+                />
+
+                <Image
+                  src={heroIocns}
+                  width={50}
+                  height={50}
+                  alt="hero-card-images "
+                  className="z-0"
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroCard
+export default HeroCard;
