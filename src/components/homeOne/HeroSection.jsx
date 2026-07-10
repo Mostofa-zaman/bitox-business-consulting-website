8d225f69ced6ff456c534fa781fdc44790055830
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-11 00:35:30 +0600

    feat(hero): add hero section layout with rotating badge

--- src/components/homeOne/HeroSection.jsx
+++ src/components/homeOne/HeroSection.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 import Container from "../common/Container";
+import RotatingBadge from '../common/RotatingBadge';
 
 
 const HeroSection = () => {
   return (
     <section>
   <Container>
-    <h1>Business Strategy</h1>
+      {/* lg, xl, 2xl */}
+        <div className="relative hidden lg:block">
+          <h1 className="font-heading headingOne uppercase tracking-tight">
+            <span className="block font-extrabold">
+              <span className="flex items-center">
+                Business <RotatingBadge size="lg" />
+              </span>
+            </span>
+            <span className="block font-medium lg:pl-[360px] xl:pl-[470px] 2xl:pl-[520px]">
+              Strategy
+            </span>
+          </h1>
+        </div>
   </Container>
 </section>
   )
