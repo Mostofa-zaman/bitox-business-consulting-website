e692b0b51a2645e382d97315f72bdc09d8542004
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-11 00:29:41 +0600

    feat(hero): add hero section layout

---
+++ src/components/homeOne/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Container from "../common/Container";
+
+
+const HeroSection = () => {
+  return (
+    <section>
+  <Container>
+    <h1>Business Strategy</h1>
+  </Container>
+</section>
+  )
+}
+
+export default HeroSection
