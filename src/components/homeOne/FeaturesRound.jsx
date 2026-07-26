a7efc9a3613e9c79ef7fe80f860843d434e5d731
mostofa zaman <mostofazaman1234@gmail.com>
2026-07-27 00:33:02 +0600

    feat(featuresround): add section heading and about page CTA

--- src/components/homeOne/FeaturesRound.jsx
+++ src/components/homeOne/FeaturesRound.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from "next/image";
 import allImages from "../helper/imageProvider";
+import ButtonTwo from "../common/ButtonTwo";
+import Link from "next/link";
 
 
 const FeaturesRound = () => {
@@ -17,6 +19,16 @@ const FeaturesRound = () => {
           className=" hidden laptop:block rounded-[6px]  absolute     top-0 laptop:top-[50%] laptop:translate-y-[-50%] laptop:left-[-110px]
            left-[-260px] rotate-[-20deg] xl:left-[-150px] 2xl:left-[-240px] !h-[130px] !w-[130px] xl:!h-[170px] xl:!w-[170px] 2xl:!h-[220px] 2xl:!w-[220px] "
         />
+         <h2 className="headingTwo text-primary font-bold  text-center   ">
+          We are a strategic consulting firm focused on delivering measurable
+          strategy, growth.
+        </h2>
+        <div className="mt-[60px] flex justify-center">
+          <Link href={"/about"} className="inline-block">
+            {" "}
+            <ButtonTwo frontText={"Learn about us"} />
+          </Link>
+        </div>
       </div>
     </section>
   )
