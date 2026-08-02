07541b99c6848beaefb7ab053578665e27535055
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-02 23:59:12 +0600

    feat(VideoShowcase): implement VideoShowcase with responsive image layout

--- src/components/homeOne/VideoShowcase.jsx
+++ src/components/homeOne/VideoShowcase.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Container from "../common/Container";
+import Image from "next/image";
+import allImages from "../helper/imageProvider";
 
 const VideoShowcase = () => {
+  const { videoShowcase } = allImages;
   return (
-    <div>VideoShowcase</div>
-  )
-}
+    <section className="py-[40px] sm:py-[60px] md:py-[80px] lg:py-[100px] xl:py-[120px]">
+      <Container size={"lg"}>
+        <div className="w-full">
+          <Image
+            width={1600}
+            height={690}
+            src={videoShowcase}
+            alt="video showcase"
+            className="w-full h-auto rounded-[4px] sm:rounded-[5px] md:rounded-[6px]"
+          />
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-export default VideoShowcase
+export default VideoShowcase;
