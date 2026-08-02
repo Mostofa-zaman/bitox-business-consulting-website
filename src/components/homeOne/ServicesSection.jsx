3218d06112274ba70a67c55ba656f95f73597986
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-02 23:04:03 +0600

    feat(services): add ServicesSection component structure

--- src/components/homeOne/ServicesSection.jsx
+++ src/components/homeOne/ServicesSection.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
-const ServicesSection = () => {
+import Container from "../ui/Container";
+
+
+export default function ServicesSection() {
   return (
-    <div>ServicesSection</div>
-  )
+    <section className=" ">
+      <Container size="lg">
+        <h2 className="text-primary headingTwo font-bold max-w-[803px] mx-auto text-center mb-[60px]">
+          {" "}
+          Get the most valuable expert service now
+        </h2>
+     
+      </Container>
+    </section>
+  );
 }
-
-export default ServicesSection
