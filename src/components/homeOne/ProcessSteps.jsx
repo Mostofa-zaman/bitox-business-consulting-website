be5fc1e9d5b46f6b52c0d59b322c2944e512a615
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-06 00:33:37 +0600

    feat(processSteps):add  ProcessSteps section with heading

--- src/components/homeOne/ProcessSteps.jsx
+++ src/components/homeOne/ProcessSteps.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const ProcessSteps = () => {
   return (
-    <div>ProcessSteps</div>
+    <section className="py-[60px] md:py-[80px] lg:py-[120px] bg-primary">
+      <Container size={"lg"}>
+        <div>
+             <h2 className="headingTwo text-center max-w-[944px] font-bold text-bg-secondaryTwo mx-auto">
+            3 Simple Steps to Accelerate Your Business Growth
+          </h2>
+
+        </div>
+      </Container>
+    </section>
   )
 }
 
