58444f45004148ea9c38a5029d53af2401626421
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-06 00:32:21 +0600

    feat(homeOne): create ProcessSteps component

--- src/components/homeOne/PortfolioSection.jsx
+++ src/components/homeOne/PortfolioSection.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-
 import Container from "../common/Container";
+import ProjectCard from "../common/ProjectCard";
 import Responsive from "../common/Responsive";
 import allImages from "../helper/imageProvider";
 import Link from "next/link";
-import ProjectCard from "../common/ProjectCard";
 
 const PortfolioSection = () => {
-    
   const { projectImages } = allImages;
   return (
     <section className="  py-[70px] md:py-[120px]">
@@ -15,12 +13,12 @@ const PortfolioSection = () => {
         <h2 className="headingTwo max-w-[950px]  text-primary font-bold text-center mx-auto">
           Real Challenges. Real Solutions. Real Impact.
         </h2>
-         <div className="py-[60px] space-y-[30px]">
-            <Responsive.Grid cols={{ base: 1, lg: 2 }} gap="lg">
+        <div className="py-[60px] space-y-[30px]">
+          <Responsive.Grid cols={{ base: 1, lg: 2 }} gap="lg">
             {projectImages[0].map((items, index) => {
               return (
                 <Link key={index} href={`/projects/${items.slug}`}>
-                   <ProjectCard
+                  <ProjectCard
                     image={items.img}
                     mainClass={"w-full !h-[400px] md:!h-[510px]"}
                     title={items.title}
@@ -31,7 +29,7 @@ const PortfolioSection = () => {
               );
             })}
           </Responsive.Grid>
-             <Responsive.Grid cols={{ base: 1, lg: 3 }} gap="lg">
+          <Responsive.Grid cols={{ base: 1, lg: 3 }} gap="lg">
             {projectImages[1].map((items, index) => {
               return (
                 <Link key={index} href={`/projects/${items.slug}`}>
@@ -46,9 +44,8 @@ const PortfolioSection = () => {
               );
             })}
           </Responsive.Grid>
-
-         </div>
-          <div>
+        </div>
+        <div>
           <Link href={"/projects"}>
             <button className="w-full cursor-pointer text-center py-5  text-primary font-medium text-base border border-[#0000001a] rounded-[6px  ]">
               See all case studies
