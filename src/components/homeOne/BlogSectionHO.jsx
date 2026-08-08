0d626d02bba4bf9b40f9622209f936e109c64ad3
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-08 23:23:52 +0600

    feat(blogSectionHO): add responsive blog grid with data

--- src/components/homeOne/BlogSectionHO.jsx
+++ src/components/homeOne/BlogSectionHO.jsx
@@ -12,7 +12,15 @@ const BlogSectionHO = () => {
         <h2 className="max-w-[800px] text-center mx-auto font-bold text-primary headingTwo">
           Insights & Updates: Driving Your Success
         </h2>
-          
+           <div className="py-[60px]">
+          <Responsive.Grid cols={{ base: 1, lg: 3 }} gap="lg">
+            {allBlogData.slice(0, 3).map((item) => {
+              return (
+            <h1>jij</h1>
+              );
+            })}
+          </Responsive.Grid>
+        </div>
        
       </Container>
     </section>
