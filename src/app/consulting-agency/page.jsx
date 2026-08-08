05e3fb2d041a4a3e843e379a5f5019f3831f0ca4
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-09 01:16:21 +0600

    feat(heroTwo): add background image and dark overlay

--- src/app/consulting-agency/page.jsx
+++ src/app/consulting-agency/page.jsx
@@ -1,10 +1,12 @@
 import ThemeInitializer from '@/components/helper/ThemeInitializer'
+import Herotwo from '@/components/homeTwo/Herotwo'
 import React from 'react'
 
 const page = () => {
   return (
     <>
       <ThemeInitializer pathName="consulting-agency" />
+      <Herotwo/>
     
     </>
   )
--- src/components/homeTwo/Herotwo.jsx
+++ src/components/homeTwo/Herotwo.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import allImages from '../helper/imageProvider'
+import Image from 'next/image'
 
 const Herotwo = () => {
   return (
-    <div>Herotwo</div>
+     <section className="relative h-[420px] sm:h-[600px] md:h-[750px] xl:h-[950px] w-full overflow-hidden">
+            {/* Background Image */}
+      <Image
+        src={allImages.home2heroBanner}
+        alt="Hero Background"
+        fill
+        priority
+        className="object-cover object-center"
+      />
+
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/40 z-[1]" />
+    </section>
   )
 }
 
