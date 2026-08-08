a25b144e660210d7e7bb52049eb8856a229ea636
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-09 01:10:22 +0600

    feat(consultingAgency): initialize page with theme setup

---
+++ src/app/consulting-agency/page.jsx
@@ -0,0 +1,13 @@
+import ThemeInitializer from '@/components/helper/ThemeInitializer'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <ThemeInitializer pathName="consulting-agency" />
+    
+    </>
+  )
+}
+
+export default page
