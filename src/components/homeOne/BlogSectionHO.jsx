9ab4c1c53dddc35e94e594c47b6be9d60b63286b
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-08 23:28:13 +0600

    feat(blogSectionHO): complete blog section with cards and navigation button

--- src/components/homeOne/BlogSectionHO.jsx
+++ src/components/homeOne/BlogSectionHO.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import Container from "../common/Container"
-import Responsive from "../common/Responsive"
-import { allBlogData } from '../helper/blogpagehelper'
-import BlogCard from '../common/BlogCard'
-
-
+import React from "react";
+import Container from "../common/Container";
+import BlogCard from "../common/BlogCard";
+import Responsive from "../common/Responsive";
+import Link from "next/link";
+import { allBlogData } from "../helper/blogpagehelper";
 
 const BlogSectionHO = () => {
   return (
-     <section className=" py-[70px] lg:py-[120px]">
+    <section className=" py-[70px] lg:py-[120px]">
       <Container size={"lg"}>
         <h2 className="max-w-[800px] text-center mx-auto font-bold text-primary headingTwo">
           Insights & Updates: Driving Your Success
         </h2>
-           <div className="py-[60px]">
+        <div className="py-[60px]">
           <Responsive.Grid cols={{ base: 1, lg: 3 }} gap="lg">
             {allBlogData.slice(0, 3).map((item) => {
               return (
-              <BlogCard
+                <BlogCard
                   batchName={item.batchName}
                   description={item.description}
                   dateText={item.dateText}
@@ -30,10 +29,16 @@ const BlogSectionHO = () => {
             })}
           </Responsive.Grid>
         </div>
-       
+        <div className=" mt-[20px] md:mt-[30px] lg::mt-[60px]">
+          <Link href={"/blog"}>
+            <button className="w-full cursor-pointer text-center py-5  text-primary font-medium text-base border border-[#0000001a] rounded-[6px]">
+              See all case studies
+            </button>
+          </Link>
+        </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default BlogSectionHO
+export default BlogSectionHO;
