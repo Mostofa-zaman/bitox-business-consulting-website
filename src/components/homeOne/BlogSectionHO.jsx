72ee6c031d5459aed2fd9ac368705f5548869803
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-08 23:13:46 +0600

    feat(blogSectionHO): add section layout and heading

--- src/components/homeOne/BlogSectionHO.jsx
+++ src/components/homeOne/BlogSectionHO.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const BlogSectionHO = () => {
   return (
-    <div>BlogSectionHO</div>
+     <section className=" py-[70px] lg:py-[120px]">
+      <Container size={"lg"}>
+        <h2 className="max-w-[800px] text-center mx-auto font-bold text-primary headingTwo">
+          Insights & Updates: Driving Your Success
+        </h2>
+      </Container>
+    </section>
   )
 }
 
