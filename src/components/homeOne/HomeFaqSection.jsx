d9067cfeffa10a5901ec2f7e30646f83643c065f
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-08 23:47:11 +0600

    feat(homeFaqSection): add FAQ section container and heading

--- src/components/homeOne/HomeFaqSection.jsx
+++ src/components/homeOne/HomeFaqSection.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Container from "../common/Container";
 
 const HomeFaqSection = () => {
   return (
-    <div>HomeFaqSection</div>
-  )
-}
+    <section className="py-[120px]">
+      <Container size="lg">
+        <div>
+          <h2 className="max-w-[981px] headingTwo text-primary font-bold text-center mx-auto mb-[80px]">
+            Frequently Asked Questions: Clear, Concise Answers
+          </h2>
+         
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-export default HomeFaqSection
+export default HomeFaqSection;
