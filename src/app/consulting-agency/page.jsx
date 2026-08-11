d3711c948402def4ac24cf1a11a26854c9478ba5
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-11 19:56:52 +0600

    feat(AboutUs):add responsive AboutUs section with hero image

--- src/app/consulting-agency/page.jsx
+++ src/app/consulting-agency/page.jsx
@@ -1,4 +1,5 @@
 import ThemeInitializer from '@/components/helper/ThemeInitializer'
+import AboutUs from '@/components/homeTwo/AboutUs'
 import Herotwo from '@/components/homeTwo/Herotwo'
 import OurClients from '@/components/homeTwo/OurClients'
 import React from 'react'
@@ -9,6 +10,7 @@ const page = () => {
       <ThemeInitializer pathName="consulting-agency" />
       <Herotwo/>
       <OurClients/>
+      <AboutUs/>
     
     </>
   )
--- src/components/homeTwo/AboutUs.jsx
+++ src/components/homeTwo/AboutUs.jsx
@@ -1,8 +1,28 @@
+"use client";
+
 import React from 'react'
+import Container from "@/components/common/Container";
+import Image from 'next/image';
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+       <section className="w-full overflow-hidden bg-white">
+      <Container size="2xl" className="px-0!">
+        <div className="flex flex-col lg:grid lg:grid-cols-[minmax(0,830px)_1fr] min-h-[500px] lg:min-h-[650px] xl:min-h-[700px]">
+           {/* Left: Hero Image */}
+          <div className="relative w-full h-[300px] sm:h-[400px] md:h-[480px] lg:h-auto lg:min-h-[600px]">
+            <Image
+              src="/images/home2_aboutus/about1.png"
+              alt="Team collaboration"
+              fill
+              priority
+              className="object-cover object-center"
+              sizes="(max-width: 1024px) 100vw, 830px"
+            />
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
