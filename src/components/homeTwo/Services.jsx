22ba606f5ab29a328a3e1d1e3e1bc563ad7820f4
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-12 00:42:13 +0600

    feat(services): add service card image component and layout structure

--- src/components/homeTwo/Services.jsx
+++ src/components/homeTwo/Services.jsx
@@ -2,10 +2,27 @@
 
 import React from 'react'
 import { services } from '../helper/helpers';
-
 import Container from "../common/Container";
 import { Stack } from "../common/Responsive";
+import allImages from "../helper/imageProvider";
+import Image from "next/image";
 
+
+// Image alda export
+export const ServiceCardImage = ({ service, index }) => {
+   
+  console.log(index);
+  
+  return (
+    <div className="w-full md:w-1/2 shrink-0">
+      <Image
+        src={allImages.home2Section[index]?.img}
+        alt={service.title}
+        className="w-full h-65 sm:h-85 md:h-[465px] object-cover md:rounded-tl-md md:rounded-bl-md transition-transform duration-300 transform-gpu"
+      />
+    </div>
+  );
+};
 
 
 const ServiceCard = ({ service }) => {
