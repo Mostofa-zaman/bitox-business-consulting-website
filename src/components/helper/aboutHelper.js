48f3810e69d84f8a18c4c7fa0484c8ce4d254c42
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-24 00:19:18 +0600

    feat(aboutHelper): add about stats three

--- src/components/helper/aboutHelper.js
+++ src/components/helper/aboutHelper.js
@@ -12,3 +12,18 @@ export const aboutStatsTwo = [
     description: "Growth-focused innovative solutions.",
   },
 ];
+
+export const aboutStatsThree = [
+  {
+    id: 1,
+    value: "+36%",
+    title: "Conversion",
+    description: "Conversion after redesign (90 days).",
+  },
+  {
+    id: 2,
+    value: "+41%",
+    title: "Sign-Ups",
+    description: "Increase in registrations from organic/paid.",
+  },
+];
