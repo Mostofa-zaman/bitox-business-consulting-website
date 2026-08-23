8714016633c36117871cb77bfbdc5bd5477fc64e
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-24 00:12:52 +0600

    feat(aboutHelper.js): add about stats helper data

---
+++ src/components/helper/aboutHelper.js
@@ -0,0 +1,14 @@
+export const aboutStatsTwo = [
+  {
+    id: 1,
+    value: "30K",
+    title: "Customers",
+    description: "Happy customers we have world-wide.",
+  },
+  {
+    id: 2,
+    value: "310+",
+    title: "Growth-Focused",
+    description: "Growth-focused innovative solutions.",
+  },
+];
