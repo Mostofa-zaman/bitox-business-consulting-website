2155b155eb9bdefcd47ba26d55d6fb359b6a2e57
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-24 00:01:28 +0600

    feat (pages): add dynamic navbar and footer layout based on pathname

---
+++ src/app/(pages)/layout.jsx
@@ -0,0 +1,24 @@
+import FooterOne from "@/components/layout/FooterOne";
+import BusinessFooterSection from "@/components/layout/FooterTwo";
+import NavbarOne from "@/components/layout/NavbarOne";
+import NavbarTwo from "@/components/layout/NavbarTwo";
+import { cookies } from "next/headers";
+
+export default async function Layout({ children }) {
+  const cookieStore = await cookies();
+
+  const getLayoutPath =
+    cookieStore.get("pathname")?.value || "business-consultant";
+
+    
+    const isConsulting = getLayoutPath === "consulting-agency";
+   
+    
+  return (
+    <>
+      {isConsulting ? <NavbarTwo /> : <NavbarOne />}
+      <main className={` bg-bg-secondaryOne ${isConsulting ?  "pt-[100px]"  : "pt-[110px]"}`}>{children}</main>
+      {isConsulting ? <BusinessFooterSection /> : <FooterOne />}
+    </>
+  );
+}
