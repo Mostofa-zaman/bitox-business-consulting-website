7a4e43968a01ec56b389ac9439c03eb5bb607881
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-24 00:15:13 +0600

    feat(AboutOverview): integrate about stats into overview

--- src/components/aboutUs/AboutOverview.jsx
+++ src/components/aboutUs/AboutOverview.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import Container from '../common/Container'
+import React from "react";
+import Container from "../common/Container";
+import CountUpStat from "../common/CountUpStat";
+import { aboutStatsTwo } from "../helper/aboutHelper";
+import Responsive from "../common/Responsive";
 
 const AboutOverview = () => {
   return (
     <section className="py-12 sm:py-16 md:py-20 lg:py-[120px]   ">
       <Container size={"lg"}>
         <div className="">
-
-
-            </div>
-    </Container>
+          <Responsive.Flex>
+            {aboutStatsTwo.map((items, index) => (
+              <CountUpStat
+                key={index}
+                value={items.value}
+                label={items.title}
+                description={items.description}
+              />
+            ))}
+          </Responsive.Flex>
+        </div>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default AboutOverview
+export default AboutOverview;
