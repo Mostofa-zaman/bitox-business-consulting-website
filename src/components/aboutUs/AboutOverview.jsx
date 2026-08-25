6ce7c2007963d41cdf3a8b74ca4504bb0cf5793f
mostofa zaman <mostofazaman1234@gmail.com>
2026-08-25 23:59:51 +0600

    feat(AboutOverview): add about us banner image to overview section

--- src/components/aboutUs/AboutOverview.jsx
+++ src/components/aboutUs/AboutOverview.jsx
@@ -20,42 +20,43 @@ const AboutOverview = () => {
             ))}
           </Responsive.Flex>
         </div>
-         <h1 className="font-bold text-[clamp(2.5rem,10vw,140px)] leading-[120%]  text-primary text-center lg:text-start py-10 md:py-[50px] lg:py-[60px]">
+        <h1 className="font-bold text-[clamp(2.5rem,10vw,140px)] leading-[120%]  text-primary text-center lg:text-start py-10 md:py-[50px] lg:py-[60px]">
           Established in 2013
         </h1>
-         <div className="flex-1 mt-4 lg:mt-0 hidden sm:block">
-              <Responsive.Flex wrap={false}>
-                {aboutStatsThree.map((items, index) => (
-                  <CountUpStat
-                    key={index}
-                    value={items.value}
-                    label={items.title}
-                    description={items.description}
-                    className="flex flex-col md:items-end w-full md:w-fit"
-                    labelClass="font-semibold text-primary mt-[12px] md:text-end"
-                    descClass="text-tarnary para-sm mt-[8px] md:text-end"
-                  />
-                ))}
-              </Responsive.Flex>
-            </div>
-             <div className="flex-1 mt-4 lg:mt-0 block sm:hidden">
-              <Responsive.Flex wrap={true}>
-                {aboutStatsThree.map((items, index) => {
-                  return (
-                    <AboutStatCard
-                      key={index}
-                      description={items.description}
-                      value={items.value}
-                      title={items.title}
-                      className={" flex flex-col md:items-end w-full md:w-fit"}
-                      childClassOne={"md:text-end"}
-                      childClassTwo={"md:text-end"}
-                    />
-                  );
-                })}
-              </Responsive.Flex>
-            </div>
-                <div className="mt-12 sm:mt-16 lg:mt-[120px] mb-8 sm:mb-10 lg:mb-[60px] ">
+        <div className="flex-1 mt-4 lg:mt-0 hidden sm:block">
+          <Responsive.Flex wrap={false}>
+            {aboutStatsThree.map((items, index) => (
+              <CountUpStat
+                key={index}
+                value={items.value}
+                label={items.title}
+                description={items.description}
+                className="flex flex-col md:items-end w-full md:w-fit"
+                labelClass="font-semibold text-primary mt-[12px] md:text-end"
+                descClass="text-tarnary para-sm mt-[8px] md:text-end"
+              />
+            ))}
+          </Responsive.Flex>
+        </div>
+        <div className="flex-1 mt-4 lg:mt-0 block sm:hidden">
+          <Responsive.Flex wrap={true}>
+            {aboutStatsThree.map((items, index) => {
+              return (
+                <AboutStatCard
+                  key={index}
+                  description={items.description}
+                  value={items.value}
+                  title={items.title}
+                  className={" flex flex-col md:items-end w-full md:w-fit"}
+                  childClassOne={"md:text-end"}
+                  childClassTwo={"md:text-end"}
+                />
+              );
+            })}
+          </Responsive.Flex>
+        </div>
+
+         <div className="mt-12 sm:mt-16 lg:mt-[120px] mb-8 sm:mb-10 lg:mb-[60px] ">
           <div className="  mb-8 md:mb-12 lg:mb-0 lg:flex   justify-between space-y-[30px] md:space-y-[50px] lg:space-y-0 lg:border-t-2 lg:border-primary relative ">
             <span className=" absolute top-[-11.7px] left-[-12px]  text-[20px] hidden lg:block">
               {triangleRight}
@@ -91,8 +92,17 @@ const AboutOverview = () => {
               </p>
             </div>
           </div>
-         
+          <div>
+              <Image
+                src={aboutUsBanner}
+                width={1600}
+                height={750}
+                className="rounded-[10px] object-cover"
+                alt="aboutUsBanner"
+              />
+            </div>
         </div>
+
       </Container>
     </section>
   );
