0457f98d9c533d3fc6c2dbad4d5e7d05efc6b97d
mostofa zaman <mostofazaman1234@gmail.com>
2026-09-04 23:41:22 +0600

    feat(pricing): add InnerPageHero section

--- src/app/(pages)/pricing/page.jsx
+++ src/app/(pages)/pricing/page.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <>
+      <InnerPageHero title="Bitox for Complete Solutions" />
+    </>
+  );
+};
 
-export default Pricing
+export default Pricing;
