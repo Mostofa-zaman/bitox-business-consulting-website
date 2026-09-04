26d9274d9408e96d92ee6988eb17fae4222b7600
mostofa zaman <mostofazaman1234@gmail.com>
2026-09-04 23:44:51 +0600

    feat(pricing): add PricingPlan to Pricing page

--- src/app/(pages)/pricing/page.jsx
+++ src/app/(pages)/pricing/page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import InnerPageHero from "@/components/common/InnerPageHero";
+import PricingPlan from "@/components/homeTwo/PricingPlan";
+
 
 const Pricing = () => {
   return (
     <>
       <InnerPageHero title="Bitox for Complete Solutions" />
+      <PricingPlan />
     </>
   );
 };
