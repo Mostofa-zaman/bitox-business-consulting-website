e736a91c9ef9525feddb1cf12ada6ece1882fa85
mostofa zaman <mostofazaman1234@gmail.com>
2026-09-05 00:15:55 +0600

    feat(FaqSection):add FAQ section layout with faq heading

--- src/components/faqSection/FaqSection.jsx
+++ src/components/faqSection/FaqSection.jsx
@@ -1,11 +1,12 @@
 import Container from "../common/Container";
+import FaqHeading from "./FaqHeading";
 
 
 const FaqSection = () => {
   return (
     <section className="py-12 sm:py-16 md:py-20 lg:py-[120px]">
       <Container size="lg">
-       <h1>hwello</h1>
+      <FaqHeading/>
       </Container>
     </section>
   );
